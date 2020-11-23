a96d2f94b4d4308016669e1d7849c03a8fd6f0c3
Weslley <weslleydrawlaw@gmail.com>
2020-11-23 20:38:27 -0300

    Primeiro Commit
    Veiculo, s/config de servidor

---
+++ LocadoraWeb/Controllers/VeiculoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocadoraWeb.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar() => View();
+        [HttpPost]
+        public IActionResult Cadastrar(string txtMarca, string txtModelo, string txtPlaca, 
+            string txtRenavan, string txtTipoCategoria, string txtTipoCombustivel, string txtCor, 
+            int txtQntdPortas, int txtAno, double txtPotencia)
+        {
+               return RedirectToAction("Index", "Veiculo");
+        }
+
+    }
+}
---
+++ LocadoraWeb/Models/Veiculo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocadoraWeb.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo : BaseModel
+    {
+        //string txtMarca, string txtModelo, string txtPlaca,
+        //    string txtRenavan, string txtTipoCategoria, string txtTipoCombustivel, string txtCor,
+        //    int txtQntdPortas, int txtAno, double txtPotencia
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public string Renavan { get; set; }
+        public string TipoCategoria { get; set; }
+        public string TipoCombustivel { get; set; }
+        public string Cor { get; set; }
+        public int QntdPortas { get; set; }
+        public int Ano { get; set; }
+        public double Potencia { get; set; }
+    }
+}
