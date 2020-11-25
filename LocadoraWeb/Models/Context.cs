9a24c34c22763ce2b8efa34857348149b7754223
Weslley <weslleydrawlaw@gmail.com>
2020-11-25 19:06:30 -0300

    Save e List de Veiculo
    Salvando dados no DB e listando os dados na Index

--- LocadoraWeb/Models/Context.cs
+++ LocadoraWeb/Models/Context.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LocadoraWeb.Models
 {
-    public class Context
+    public class Context : DbContext
     {
+        public Context(DbContextOptions options): base(options) { }
+        
+        public DbSet<Veiculo> Veiculos { get; set; }
     }
 }
